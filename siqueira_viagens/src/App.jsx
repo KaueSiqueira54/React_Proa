52cb827a47d0e71b7a7a6d513533e44256b75747
KaueSiqueira <kaue54150@gmail.com>
2026-04-24 12:40:28 -0300

    Aula react dia 24_04 - agencia - continuação

--- siqueira_viagens/src/App.jsx
+++ siqueira_viagens/src/App.jsx
@@ -2,12 +2,20 @@ import "./css/App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Escocia from "./components/Escocia";
+import Aruba from "./components/Aruba";
+import Muralha from "./components/Muralha";
+import Canyon from "./components/Canyon";
 
 function App() {
   return (
     <main>
       <Header />
       <Home />
+      <Escocia />
+      <Aruba />
+      <Muralha />
+      <Canyon />
       <Footer />
     </main>
   );
